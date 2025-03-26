3853abd58b765c2613f0e9b125a15896cff27024
Rubil <mogererubil@gmail.com>
2025-03-26 09:56:43 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+// Function to find the year of a Super Bowl win
+function superbowlWin(record) {
+    let win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+
+// Export the function
+module.exports = {
+    superbowlWin
+};
